c1c918bfac72860ec219862a397ba0b6d37559ad
cecilyshen <ccaayou@gmail.com>
2019-10-26 12:58:39 +1100

    Revert "Markdown"

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as style from './style';
-import { Table } from './components/table';
-import ReactMarkdown from 'react-markdown';
+import { Table } from './components/table'
 
-const App = (props) => {
-  const [markdown, setMarkdown] = useState('Please write an essay about your favorite DOM element.');
-
-  const handleChange = ({target}) => {
-    setMarkdown(target.value);
-  };
-
+function App() {
   return (
-    <body className="App">
-      <div className="container">
-        <div className="col-markdown">
-          <textarea className="markdown-textarea" type="text" name="input" value={markdown} onChange={handleChange}></textarea>
-        </div>
-        <div className="col-preview">
-          <Table style={style.outerTableStyle}>
-          <tbody>
-            <tr>
-                <td data-testid="child">hello</td>
-            </tr>
-          </tbody>
-          </Table>
-          <ReactMarkdown source={markdown} />
-        </div>
-      </div>
+    <body className="App" style={style.bodyStyle}>
+      <Table style={style.outerTableStyle}>
+        <tbody>
+          <tr>
+              <td data-testid="child">hello</td>
+          </tr>
+        </tbody>
+      </Table>
     </body>
   );
-};
+}
 
 export default App;
