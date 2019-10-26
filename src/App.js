0761c4b6c122c4a35e0153a2371067052b848fb1
Cecily Shen <cecilyshen@Cecilys-MacBook-Pro.local>
2019-10-26 16:02:40 +1100

    trying styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { useState } from 'react';
 import * as style from './style';
 import { Table } from './components/table';
 import ReactMarkdown from 'react-markdown';
+import htmlParser from 'react-markdown/plugins/html-parser';
+
+const parseHtml = htmlParser({
+  isValidNode: node => {
+  
+  console.log(node);
+  return node.type !== 'script';
+  },
+  processingInstructions: [{
+    // Custom <h1> processing
+    shouldProcessNode: function (node) {
+      return node.parent && node.parent.name && node.parent.name === 'h1';
+    },
+    processNode: function (node, children) {
+      console.log(node);
+      return node.data.toUpperCase();
+    }
+  },]
+})
 
 const App = (props) => {
   const [markdown, setMarkdown] = useState('Please write an essay about your favorite DOM element.');
@@ -17,15 +36,18 @@ const App = (props) => {
           <textarea className="markdown-textarea" data-testid="textarea" type="text" name="input" value={markdown} onChange={handleChange}></textarea>
         </div>
         <div className="col-preview">
-          <Table style={style.outerTableStyle}>
+          {/* <Table style={style.outerTableStyle}>
           <tbody>
             <tr>
                 <td data-testid="child">hello</td>
             </tr>
           </tbody>
-          </Table>
+          </Table> */}
           <div data-testid="preview">
-            <ReactMarkdown source={markdown} />
+            <ReactMarkdown
+              source={markdown}
+              astPlugins={[parseHtml]}
+              escapeHtml={false}/>
           </div>
         </div>
       </div>
--- src/components/h1.js
+++ src/components/h1.js
@@ -1,3 +1,4 @@
 import React from 'react'
+import 'H1.css'
 
 export const H1 = ({h1Style, children, ...props}) => <h1 style={h1Style} {...props}>{children}</h1>
