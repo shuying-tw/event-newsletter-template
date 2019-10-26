a8c5bbe1c45ccf0067195131b1f8202df2507ab9
Cecily Shen <cecilyshen@Cecilys-MacBook-Pro.local>
2019-10-26 12:56:36 +1100

    add markdown support for textarea

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as style from './style';
-import { Table } from './components/table'
+import { Table } from './components/table';
+import ReactMarkdown from 'react-markdown';
 
-function App() {
+const App = (props) => {
+  const [markdown, setMarkdown] = useState('Please write an essay about your favorite DOM element.');
+
+  const handleChange = ({target}) => {
+    setMarkdown(target.value);
+  };
+
   return (
-    <body className="App" style={style.bodyStyle}>
-      <Table style={style.outerTableStyle}>
-        <tbody>
-          <tr>
-              <td data-testid="child">hello</td>
-          </tr>
-        </tbody>
-      </Table>
+    <body className="App">
+      <div className="container">
+        <div className="col-markdown">
+          <textarea className="markdown-textarea" type="text" name="input" value={markdown} onChange={handleChange}></textarea>
+        </div>
+        <div className="col-preview">
+          <Table style={style.outerTableStyle}>
+          <tbody>
+            <tr>
+                <td data-testid="child">hello</td>
+            </tr>
+          </tbody>
+          </Table>
+          <ReactMarkdown source={markdown} />
+        </div>
+      </div>
     </body>
   );
-}
+};
 
 export default App;
